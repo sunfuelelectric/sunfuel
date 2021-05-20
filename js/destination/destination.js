41af873f97e91cd7e4f9dfb5e27b6bbdde13ec7f
Ayush <ayush.kumar20@yahoo.in>
2021-05-20 22:32:36 +0530

    update

--- js/destination/destination.js
+++ js/destination/destination.js
@@ -1,23 +1,23 @@
 gsap.registerPlugin(ScrollTrigger);
 
-const panels = gsap.utils.toArray("#panels-container .panel");
+// const panels = gsap.utils.toArray("#panels-container .panel");
 
-gsap.to(panels, {
-  xPercent: -100 * (panels.length - 1),
-  ease: "none",
-  scrollTrigger: {
-    trigger: "#panels-container",
-    pin: "#panels",
-    start: "top top",
-    scrub: .5,
-    snap: {
-      snapTo: 1 / (panels.length - 1),
-      duration: { min: 1, max: 1 }
-    },
-    end: () => "+=" + document.querySelector("#panels-container").offsetWidth,
-    autoAlpha: 0
-  }
-});
+// gsap.to(panels, {
+//   xPercent: -100 * (panels.length - 1),
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: "#panels-container",
+//     pin: "#panels",
+//     start: "top top",
+//     scrub: .5,
+//     snap: {
+//       snapTo: 1 / (panels.length - 1),
+//       duration: { min: 1, max: 1 }
+//     },
+//     end: () => "+=" + document.querySelector("#panels-container").offsetWidth,
+//     autoAlpha: 0
+//   }
+// });
 
 gsap.utils.toArray(".up_reveal").forEach(function (elem) {
   gsap.from(elem, {
@@ -77,6 +77,21 @@ gsap.utils.toArray(".up_reveal_first").forEach(function (elem) {
     duration: 2
   })
 });
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("panel");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.opacity="0";
+  }
+  myIndex++;
+  if (myIndex > x.length) { myIndex = 1 }
+  x[myIndex - 1].style.opacity="1";
+  setTimeout(carousel, 6000);
+}
 
 document.getElementById("ctrlOne").onclick = function () {
   document.getElementById("ctrlOne").classList.remove("btnCtrlGeneric");
